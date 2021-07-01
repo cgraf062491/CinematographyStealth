c048c41441f31498be5b528bc010eac63a793859
cgraf062491 <cgraf062491@gmail.com>
2021-07-01 17:11:23 -0400

    Christopher Graf - 07/01/21 - Security Cameras and Sleeping Guard Interaction
    Compelted both the Security Cameras and Sleeping Guard Interaction sections. Apologies, I forgot to commit after the Security Cameras section

---
+++ Assets/The Great Fleece/Game/Scripts/GrabKeycardActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeycardActivation : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _grabCardCutscene;
+
+    void OnTriggerEnter(Collider other)
+    {
+    	if(other.CompareTag("Player"))
+    	{
+    		_grabCardCutscene.SetActive(true);
+    	}
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameOverCutscene;
+
+    private Animator _anim;
+    private Renderer _renderer;
+
+    void Start()
+    {
+    	_anim = transform.parent.GetComponent<Animator>();
+    	_renderer = GetComponent<Renderer>();
+    }
+
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+    	if(other.CompareTag("Player"))
+    	{
+    		_anim.enabled = false;
+    		_renderer.material.SetColor("_TintColor", new Color(0.43f, 0.1f, 0.12f, 0.04f));
+    		yield return new WaitForSeconds(0.5f);
+    		//Debug.Log("eyes");
+    		_gameOverCutscene.SetActive(true);
+    	}
+    }
+}
