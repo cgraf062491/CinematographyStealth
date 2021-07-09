70d3f36109bdc9400d4c325708f4a6af4d629106
cgraf062491 <cgraf062491@gmail.com>
2021-07-09 14:39:03 -0400

    Christopher Graf - Main Menu - 07/09/21
    Completed the Main Menu section of the Cinematography course

---
+++ Assets/The Great Fleece/Game/Scripts/MainMenu/LoadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+	[SerializeField] private Image progress_bar;
+
+	void Start()
+	{
+		StartCoroutine(LoadLevelAsync());
+	}
+
+	IEnumerator LoadLevelAsync()
+	{
+		yield return null;
+
+		AsyncOperation operation = SceneManager.LoadSceneAsync(2);
+
+		while(!operation.isDone)
+		{
+			progress_bar.fillAmount = operation.progress;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
